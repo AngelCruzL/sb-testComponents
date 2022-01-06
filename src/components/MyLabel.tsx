4746fd2c7d7055ab691bf6d28850039ccde65f20
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-01-06 13:17:16 -0600

    feat: adding new props to MyLabel component

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -9,11 +9,25 @@ export interface MyLabelProps {
    * This is the size of the label
    */
   size: 'normal' | 'h1' | 'h2' | 'h3';
+  /**
+   * This is the color of the label
+   */
+  color: 'primary' | 'secondary' | 'tertiary';
+  /**
+   * This is the capitalization option of the label
+   */
+  allCaps?: boolean;
 }
 
 export const MyLabel = ({
   label = 'No label',
   size = 'normal',
+  color = 'primary',
+  allCaps = false,
 }: MyLabelProps) => {
-  return <span className={`${size}`}>{label}</span>;
+  return (
+    <span className={`label ${size} text-${color}`}>
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
 };
